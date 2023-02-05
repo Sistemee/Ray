33ab860af151ce1fc8ec85d650e54856a5a5af61
Sistemee <dagedar123@gmail.com>
2023-02-04 22:42:03 -0500

    Initial commit

---
+++ Compilers.h
@@ -0,0 +1,115 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Winter, 2023
+* Author: TO_DO
+* Professors: Paulo Sousa
+************************************************************
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@                                                               @@@@
+@@          /       ==============================        /          @@
+@         ////           @@       @@ #@                  ////         @
+@         ////*          @@ @%  @ @@    @@  @ #@#       /////         @
+@     ((( ////))))   @@  @% @%  @ @@ #@ @@  @ #@@#   ///////( ///     @
+@     ((((,/ )))))    @@@@   @@@  @@ ##  @@@   #@#   ///// ,/////     @
+@     (((((/)))((    ------------------------------    ((((./////*    @
+@    //*./ /  .///   ---  PROGRAMMING LANGUAGE  ---  ////   / ( (//   @
+@    ///// / /////   ==============================  * ////* / ////   @
+@     ///// ///// ((                               (( ///// /////     @
+@    ((((   / , (((((                             (((((  //   /(((    @
+@    (((((((/ . (((((                          (((((* / (((((((       @
+@      (((((( //((((/((((                    *((((/((((/( (((((/      @
+@       .//,   * /   (((((                   (((((  ///    .//.       @
+@     ,////////./(  (((((* ////         (///(((((( ,/(,////////       @
+@         //////// ,// ((( /////,     ////// ((( //  /////// *        @
+@            (((((((((,// * /////     /////   (//(((((((((            @
+@            ((((((((((((/////         //.///  (((((((((((.           @
+@                   (///////// //(   (//.//////////                   @
+@                  (/////////             //////////                  @
+@                                                                     @
+@@          A L G O N Q U I N   C O L L E G E  -  2 0 2 3 W          @@
+@@@@                                                               @@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+*/
+
+/*
+************************************************************
+* File name: Compilers.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Assignment: A12, A22, A32.
+* Date: Jan 01 2023
+* Professor: Paulo Sousa
+* Purpose: This file defines the functions called by main function.
+* Function list: mainReader(), mainScanner(), mainParser().
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#define COMPILERS_H_
+
+#define DEBUG 0
+
+/*
+ * ............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: This file must be updated according to each assignment
+ * (see the progression: reader > scanner > parser).
+ * ............................................................................
+ */
+ 
+/* Logical constants - adapt for your language */
+#define JULIUS_TRUE  1
+#define JULIUS_FALSE 0
+
+/*
+------------------------------------------------------------
+Data types definitions
+NOTE: Some types may not be directly used by your language,
+		but they can be necessary for conversions.
+------------------------------------------------------------
+*/
+
+/* TO_DO: Define your typedefs */
+typedef void			ray_void;
+typedef char			ray_char;
+typedef int				ray_intg;
+typedef float			ray_real;
+
+typedef unsigned char	ray_boln;
+typedef unsigned char	ray_byte;
+
+typedef long			ray_long;
+typedef double			ray_doub;
+
+/*
+------------------------------------------------------------
+Programs:
+1: Reader - invokes MainReader code
+2: Scanner - invokes MainScanner code
+3: Parser - invokes MainParser code
+------------------------------------------------------------
+*/
+enum PROGRAMS {
+	PGM_READER	= 'R',
+	PGM_SCANNER = 'S',
+	PGM_PARSER	= 'P'
+};
+
+/*
+------------------------------------------------------------
+Main functions signatures
+(Code will be updated during next assignments)
+------------------------------------------------------------
+*/
+ray_intg mainReader(ray_intg argc, ray_char** argv);
+/*
+julius_intg mainScanner(julius_intg argc, julius_char** argv);
+julius_intg mainParser(julius_intg argc, julius_char** argv);
+*/
+ray_void printLogo();
+
+#endif
