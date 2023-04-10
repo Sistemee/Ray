4ab2510a11c46a56cd9ff6b9f61cd3e3b0002d5c
Sistemee <dagedar123@gmail.com>
2023-04-10 17:03:35 -0400

    start up

---
+++ Parser.h
@@ -0,0 +1,115 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Winter, 2023
+* Author: TO_DO
+* Professors: Paulo Sousa
+************************************************************
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@                                                               @@@@
+@@          /       ==============================        /          @@
+@         ////           @@       @@ #@                  ////         @
+@         ////*          @@ @%  @ @@    @@  @ #@#       /////         @
+@     ((( ////))))   @@  @% @%  @ @@ #@ @@  @ #@@#   ///////( ///     @
+@     ((((,/ )))))    @@@@   @@@  @@ ##  @@@   #@#   ///// ,/////     @
+@     (((((/)))((    ------------------------------    ((((./////*    @
+@    //*./ /  .///   ---  PROGRAMMING LANGUAGE  ---  ////   / ( (//   @
+@    ///// / /////   ==============================  * ////* / ////   @
+@     ///// ///// ((                               (( ///// /////     @
+@    ((((   / , (((((                             (((((  //   /(((    @
+@    (((((((/ . (((((                          (((((* / (((((((       @
+@      (((((( //((((/((((                    *((((/((((/( (((((/      @
+@       .//,   * /   (((((                   (((((  ///    .//.       @
+@     ,////////./(  (((((* ////         (///(((((( ,/(,////////       @
+@         //////// ,// ((( /////,     ////// ((( //  /////// *        @
+@            (((((((((,// * /////     /////   (//(((((((((            @
+@            ((((((((((((/////         //.///  (((((((((((.           @
+@                   (///////// //(   (//.//////////                   @
+@                  (/////////             //////////                  @
+@                                                                     @
+@@          A L G O N Q U I N   C O L L E G E  -  2 0 2 3 W          @@
+@@@@                                                               @@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+*/
+
+/*
+************************************************************
+* File name: parser.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Assignment: A32.
+* Date: Sep 01 2022
+* Professor: Paulo Sousa
+* Purpose: This file is the main header for Parser (.h)
+*************************************************************/
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+/* Inclusion section */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+#ifndef READER_H_
+#include "Reader.h"
+#endif
+#ifndef SCANNER_H_
+#include "Scanner.h"
+#endif
+
+/* Global vars */
+static Token lookahead;
+extern BufferReader* stringLiteralTable;
+extern ray_intg line;
+extern Token tokenizer();
+extern ray_char* keywordTable[];
+ray_intg syntaxErrorNumber = 0;
+
+#define STR_LANGNAME	"ray"
+#define LANG_WRTE		"print&"
+#define LANG_READ		"input&"
+#define LANG_MAIN		"main&"
+
+/* TO_DO: Create ALL constants for keywords (sequence given in table.h) */
+
+/* Constants */
+enum KEYWORDS {
+	NO_ATTR = -1,
+	KW_data,
+	KW_code,
+	KW_int,
+	KW_real,
+	KW_string,
+	KW_if,
+	KW_then,
+	KW_else,
+	KW_while,
+	KW_do
+};
+
+/* Function definitions */
+ray_void startParser();
+ray_void matchToken(ray_intg, ray_intg);
+ray_void syncErrorHandler(ray_intg);
+ray_void printError();
+
+/* TO_DO: Place ALL non-terminal function declarations */
+ray_void codeSession();
+ray_void dataSession();
+ray_void optVarListDeclarations();
+ray_void optionalStatements();
+ray_void outputStatement();
+ray_void outputVariableList();
+ray_void program();
+ray_void statement();
+ray_void statements();
+ray_void statementsPrime();
+
+#endif
